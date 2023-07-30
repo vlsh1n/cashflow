ebca9080e62cf05eb80de90a854e258d68de703f
Vladislav Voloshin <68925220+vlsh1n@users.noreply.github.com>
2023-07-30 15:36:28 +0300

    Update settings.py
    deleted secret key

--- financeapp/financeapp/settings.py
+++ financeapp/financeapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h04y=s69ztwo_oe@5b%4%aru9=d&r1qk71mr8#3eaa&!mqcr6^'
+SECRET_KEY = 'add'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
